32dc7b2c108b1916e07ad3b6af8cbb1d99394ec3
Pablo <358126+marce1994@users.noreply.github.com>
2021-06-03 21:51:22 +0200

    Create main.py

---
+++ Python/Class1/main.py
@@ -0,0 +1,27 @@
+output = 'hello "World"';
+
+print(output);
+
+output = "Pepe";
+
+print(type(output));
+
+print(output[0]);
+print(output[1]);
+print(output[2]);
+print(output[3]);
+
+output = "Hello " + output;
+
+print(output);
+
+output = "Pepe";
+
+print((output + " ") * 6);
+
+a = input("Enter a number: \n");
+
+print(type(a));
+print(type(int(a)));
+
+print("Your input was " + a);
