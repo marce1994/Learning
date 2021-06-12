d56af4dfd9388ca1031e30d8ed92adeda297eccb
Pablo <358126+marce1994@users.noreply.github.com>
2021-06-12 19:18:15 +0200

    Create 1_cuadrado_binomio.py

---
+++ Python/Tarea/Guia1/1_cuadrado_binomio.py
@@ -0,0 +1,5 @@
+a = int(input("Ingrese a: "));
+b = int(input("Ingrese b: "));
+
+print(f'({a}+{b})**2 = {a}**2 + {a}*{b}*2 + {b}**2');
+print((a+b)**2,"=",a**2 + a*b*2 + b**2, " => ", (a+b)**2 == a**2 + a*b*2 + b**2);
